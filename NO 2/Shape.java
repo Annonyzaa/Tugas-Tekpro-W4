fb67e09a012172f5106584710fac481cfe8586b0
Annonyzaa <syibli.fauzi@gmail.com>
2026-03-06 22:55:22 +0700

    Add files via upload

---
+++ NO 2/Shape.java
@@ -0,0 +1,17 @@
+public abstract class Shape {
+
+    protected String shapeName;
+
+    // Constructor
+    public Shape(String name) {
+        shapeName = name;
+    }
+
+    // Abstract method
+    public abstract double area();
+
+    // toString method
+    public String toString() {
+        return shapeName;
+    }
+}
